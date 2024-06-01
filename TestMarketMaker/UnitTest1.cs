9f2bde901a1008186ffab8fa7a3fb7b027585114
itbergl <tberg644@gmail.com>
2024-06-01 23:19:00 +1000

    initial commit

---
+++ TestMarketMaker/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestMarketMaker;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
